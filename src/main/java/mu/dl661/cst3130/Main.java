57ce1a4d790fcb34ed0cb14a48b2137ec20eb4bb
dereck lhw <dereck.lamhonwah@gmail.com>
2023-11-29 14:16:53 +0400

    feat: file cleanup

--- src/main/java/mu/dl661/cst3130/Main.java
+++ src/main/java/mu/dl661/cst3130/Main.java
@@ -1,20 +1,8 @@
 package mu.dl661.cst3130;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class Main {
    public static void main( String[] args ) {
       System.out.println( "Hello World!" );
-
-      Configuration config = new org.hibernate.cfg.Configuration();
-      config.configure();
-
-      SessionFactory factory = config.buildSessionFactory();
-      System.out.println(factory);
-
-      SeleniumDemo seleniumDemo = new SeleniumDemo();
-      seleniumDemo.showAsdaHTML();
    }
 
 }
