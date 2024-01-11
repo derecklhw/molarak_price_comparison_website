97f93c9a5f7681d00eac9294514d3c0daf21be11
dereck lhw <dereck.lamhonwah@gmail.com>
2024-01-11 14:49:09 +0400

    feat: change logging framework from logback to log4j

--- src/main/java/mu/dl661/cst3130/config/HibernateConfig.java
+++ src/main/java/mu/dl661/cst3130/config/HibernateConfig.java
@@ -47,8 +47,8 @@ public class HibernateConfig {
                 env.getProperty("hibernate.connection.pool_size"));
         hibernateProperties.setProperty("hibernate.current_session_context_class",
                 env.getProperty("hibernate.current_session_context_class"));
-        hibernateProperties.setProperty("hibernate.show_sql", env.getProperty("hibernate.show_sql"));
-        hibernateProperties.setProperty("hibernate.format_sql", env.getProperty("hibernate.format_sql"));
+        // hibernateProperties.setProperty("hibernate.format_sql",
+        // env.getProperty("hibernate.format_sql"));
         hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
         return hibernateProperties;
     }
