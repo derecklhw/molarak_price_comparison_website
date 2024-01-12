5b1182e3ab12b71a7210a2290c84e1660b963bd1
dereck lhw <dereck.lamhonwah@gmail.com>
2024-01-12 11:41:48 +0400

    feat: whiskyshop cant be run headless and cannot traverse into each product url, got block also implemented the structure of first scraper

--- src/main/java/mu/dl661/cst3130/scraper/WebsiteScraper1.java
+++ src/main/java/mu/dl661/cst3130/scraper/WebsiteScraper1.java
@@ -1,5 +1,20 @@
 package mu.dl661.cst3130.scraper;
 
+import java.time.Duration;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class WebsiteScraper1 extends Thread {
 
     private String url;
@@ -11,21 +26,53 @@ public class WebsiteScraper1 extends Thread {
 
     @Override
     public void run() {
-        System.out.println("Scraping Website1 " + url);
-        // Implementation of the scraping logic for Website1
+        String itemName = "blended-scotch-whisky";
 
-        // Example using Jsoup (for simpler HTML pages)
-        // Document doc = Jsoup.connect(url).get();
-        // Elements data = doc.select("CSS selector");
+        System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
-        // Example using Selenium (for JavaScript heavy pages)
-        // WebDriver driver = new ChromeDriver();
-        // driver.get(url);
-        // WebElement element = driver.findElement(By.cssSelector("CSS selector"));
+        ChromeOptions options = new ChromeOptions();
 
-        // Process the data extracted
+        options.setBinary(
+                System.getProperty("user.dir") + "/chrome-linux64/chrome");
+        // options.addArguments("--headless");
 
-        // Don't forget to close Selenium WebDriver if used
-        // driver.quit();
+        // Create instance of web driver - this must be on the path.
+        WebDriver driver = new ChromeDriver(options);
+
+        for (int page = 1; page <= 3; page++) {
+            driver.get(url + "/" + itemName + "?p=" + page + "&product_list_limit=96");
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+            wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".item.product.product-item")));
+
+            String htmlSource = driver.getPageSource();
+
+            Document doc = Jsoup.parse(htmlSource);
+            Elements prods = doc.select(".item.product.product-item");
+
+            for (Element prod : prods) {
+                Elements prodAnchorTags = prod.select("a.product.photo.product-item-photo");
+
+                for (Element prodAnchorTag : prodAnchorTags) {
+                    String websiteUrl = prodAnchorTag.attr("href");
+
+                    driver.get(websiteUrl);
+
+                    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".product-info-main")));
+
+                    String productHtmlSource = driver.getPageSource();
+
+                    Document productDoc = Jsoup.parse(productHtmlSource);
+
+                    String name = productDoc.select(".page-title-wrapper.product > h1").text();
+                    System.out.println(name);
+
+                }
+            }
+        }
+
+        // Exit driver and close Chrome
+        driver.quit();
     }
 }
--- src/main/java/mu/dl661/cst3130/threading/ThreadManager.java
+++ src/main/java/mu/dl661/cst3130/threading/ThreadManager.java
@@ -4,11 +4,11 @@ import mu.dl661.cst3130.scraper.*;
 
 public class ThreadManager {
     public void runScraperThreads() {
-        Thread website1 = new WebsiteScraper1("vf");
-        Thread website2 = new WebsiteScraper2("vf");
-        Thread website3 = new WebsiteScraper3("vf");
-        Thread website4 = new WebsiteScraper4("vf");
-        Thread website5 = new WebsiteScraper5("vf");
+        Thread website1 = new WebsiteScraper1("http://www.whiskyshop.com");
+        Thread website2 = new WebsiteScraper2("http://www.masterofmalt.com");
+        Thread website3 = new WebsiteScraper3("http://www.amazon.co.uk");
+        Thread website4 = new WebsiteScraper4("http://www.thewhiskyexchange.com");
+        Thread website5 = new WebsiteScraper5("http://www.vinatis.de");
 
         website1.start();
         website2.start();
